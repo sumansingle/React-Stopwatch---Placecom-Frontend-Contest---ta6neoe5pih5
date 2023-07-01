a84aa75cb59ea370360b1e91d72957eb42a68d0f
Suman Ghosh <90588892+sumansingle@users.noreply.github.com>
2023-07-01 19:54:38 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,54 +1,48 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState,useEffect } from 'react'
 import '../styles/App.css';
-
 const App = () => {
   const startTime = useRef(0);
   const intervalRef = useRef(0);
   const [currentTime, setCurrentTime] = useState(0);
   const [laps, setLaps] = useState([]);
-
-  const startTimer = () => {
-    startTime.current = Date.now() - currentTime;
-    intervalRef.current = setInterval(() => {
-      setCurrentTime(Date.now() - startTime.current);
-    }, 10);
-  };
-
-  const stopTimer = () => {
-    clearInterval(intervalRef.current);
-  };
-
-  const resetTimer = () => {
-    clearInterval(intervalRef.current);
-    setCurrentTime(0);
-    setLaps([]);
-  };
-
-  const addLap = () => {
-    setLaps([...laps, currentTime]);
-  };
-
+  const [running, setRunning] = useState(false);
+  const [lapvisi, setlapvisi] = useState(false);
+  useEffect(() => {
+    let interval;
+    if (running) {
+      interval = setInterval(() => {
+        setCurrentTime((prevTime) => prevTime + .010);
+      }, 10);
+    } else if (!running) {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [running]);
+    
+  function handleLap(){
+    setLaps([...laps,currentTime])
+    setlapvisi(true)
+  }
   return (
     <div id="main">
       <section>
-        <h1 className='seconds-elapsed'>{(currentTime / 1000).toFixed(2)}</h1>
+        <h1 className='seconds-elapsed'>{currentTime.toFixed(3)}</h1>
         <section className='buttons'>
-          <button className="start-btn" onClick={startTimer}>START</button>
-          <button className="stop-btn" onClick={stopTimer}>STOP</button>
-          <button className="lap-btn" onClick={addLap}>LAP</button>
-          <button className="reset-btn" onClick={resetTimer}>RESET</button>
+          <button className="start-btn" onClick={()=>{setRunning(true)}}>START</button>
+          <button className="stop-btn" onClick={()=>{setRunning(false)}}>STOP</button>
+          <button className="lap-btn" onClick={handleLap}>LAP</button>
+          <button className="reset-btn" onClick={()=>{setRunning(false) ,setCurrentTime(0),setlapvisi(false),setLaps([])}}>RESET</button>
         </section>
       </section>
-      <section className='lap-section'>
+      {lapvisi&&<section className='lap-section'>
         <h2>Laps</h2>
         <section className='laps'>
-          {laps.map((lapTime, index) => (
-            <p key={index}>{(lapTime / 1000).toFixed(2)}</p>
-          ))}
+          {laps.map((item)=> <p>{item.toFixed(3)}</p>)}
         </section>
-      </section>
+      </section>}
     </div>
-  );
-};
+  )
+}
+
 
 export default App;
