d7507b19c0651aefbc8c9a4969c20e395733eb6e
Suman Ghosh <90588892+sumansingle@users.noreply.github.com>
2023-07-01 19:50:00 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,33 +1,47 @@
-import React, { useRef, useState } from 'react'
+import React, { useRef, useState } from 'react';
 import '../styles/App.css';
+
 const App = () => {
   const startTime = useRef(0);
   const intervalRef = useRef(0);
   const [currentTime, setCurrentTime] = useState(0);
   const [laps, setLaps] = useState([]);
 
+  const startTimer = () => {
+    startTime.current = Date.now() - currentTime;
+    intervalRef.current = setInterval(() => {
+      setCurrentTime(Date.now() - startTime.current);
+    }, 10);
+  };
+
+  const stopTimer = () => {
+    clearInterval(intervalRef.current);
+  };
+
+  const resetTimer = () => {
+    clearInterval(intervalRef.current);
+    setCurrentTime(0);
+    setLaps([]);
+  };
+
+  const addLap = () => {
+    setLaps([...laps, currentTime]);
+  };
+
   return (
     <div id="main">
       <section>
-        <h1 className='seconds-elapsed'>Stopwatch Time</h1>
+        <h1 className='seconds-elapsed'>{(currentTime / 1000).toFixed(2)}</h1>
         <section className='buttons'>
-          <button className="start-btn">START</button>
-          <button className="stop-btn">STOP</button>
-          <button className="lap-btn">LAP</button>
-          <button className="reset-btn">RESET</button>
+          <button className="start-btn" onClick={startTimer}>START</button>
+          <button className="stop-btn" onClick={stopTimer}>STOP</button>
+          <button className="lap-btn" onClick={addLap}>LAP</button>
+          <button className="reset-btn" onClick={resetTimer}>RESET</button>
         </section>
       </section>
       <section className='lap-section'>
         <h2>Laps</h2>
         <section className='laps'>
-          <p>lap</p>
-          <p>lap</p>
-          <p>lap</p>
-        </section>
-      </section>
-    </div>
-  )
-}
+          {laps.map((lapTime, index) => (
+            <p key={index}>{(lapTime / 1000).toFixed(2)}</
 
-
-export default App;
